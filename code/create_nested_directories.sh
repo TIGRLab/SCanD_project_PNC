b22bea57f272388d802054b1152f8aac3da869b9
sunclara <132281878+sunclara@users.noreply.github.com>
2023-06-19 14:30:55 -0400

    Update create_nested_directories.sh
    Removed old code and added comment

--- code/create_nested_directories.sh
+++ code/create_nested_directories.sh
@@ -3,12 +3,10 @@
 base_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids"
 parent_directory_prefix="sub-"
 
-# Iterate through the sub--X directories
+# Iterate through the sub-X directories
 for sub_directory in ${base_directory}/${parent_directory_prefix}*
+# the wildcard must be outside quotation marks and brackets
 do
-    # parent_directory="${base_directory}/${parent_directory_prefix}$"
-    # base_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids"
-    # parent_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids/sub-"
     nested_directory="${sub_directory}/dwi_temp"
     
     mkdir -p "${nested_directory}"
