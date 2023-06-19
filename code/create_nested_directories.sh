5aea9c36d45f11210909a3c0dd3823cf1bc17b91
sunclara <132281878+sunclara@users.noreply.github.com>
2023-06-19 14:24:03 -0400

    Update create_nested_directories.sh

--- code/create_nested_directories.sh
+++ code/create_nested_directories.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 base_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids"
-parent_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids/sub-"
+parent_directory_prefix="sub--"
 
-for "$parent_directory"-* in base_directory
+# Iterate through the sub--X directories
+for sub_directory in ${base_directory}/${parent_directory_prefix}*
 do
     # parent_directory="${base_directory}/${parent_directory_prefix}$"
-    base_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids"
-    parent_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids/sub-"
-    nested_directory="${parent_directory}/dwi_temp"
+    # base_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids"
+    # parent_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids/sub-"
+    nested_directory="${sub_directory}/dwi_temp"
     
     mkdir -p "${nested_directory}"
     echo "Creating nested dwi_temp directories"
