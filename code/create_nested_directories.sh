0160dea39fa744e0f8cdf89cacc222ebc0127208
sunclara <132281878+sunclara@users.noreply.github.com>
2023-06-19 14:06:52 -0400

    Update create_nested_directories.sh
    trying to debug to create nested directories

--- code/create_nested_directories.sh
+++ code/create_nested_directories.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 base_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids"
-parent_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids/sub--*"
+parent_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids/sub-"
 
-for parent_directory in base_directory
+for "$parent_directory"-* in base_directory
 do
     # parent_directory="${base_directory}/${parent_directory_prefix}$"
     base_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids"
-    parent_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids/sub--*"
+    parent_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids/sub-"
     nested_directory="${parent_directory}/dwi_temp"
     
     mkdir -p "${nested_directory}"
+    echo "Creating nested dwi_temp directories"
 done
