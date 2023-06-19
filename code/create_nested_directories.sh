f310bc95db0e41c4a94c3746e9ddc6d82ccda5b8
sunclara <132281878+sunclara@users.noreply.github.com>
2023-06-19 11:48:21 -0400

    Update create_nested_directories.sh
    removed brackets around the arguments of the for loop

--- code/create_nested_directories.sh
+++ code/create_nested_directories.sh
@@ -3,7 +3,7 @@
 base_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids"
 parent_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids/sub-*"
 
-for (parent_directory in base_directory)
+for parent_directory in base_directory
 do
     # parent_directory="${base_directory}/${parent_directory_prefix}$"
     nested_directory="${parent_directory}/dwi_temp"
