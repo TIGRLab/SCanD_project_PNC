057a11d7261d254d1fe206921ceb99f1d0765fdf
sunclara <132281878+sunclara@users.noreply.github.com>
2023-06-19 13:47:11 -0400

    Update create_nested_directories.sh

--- code/create_nested_directories.sh
+++ code/create_nested_directories.sh
@@ -6,6 +6,7 @@ parent_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids
 for parent_directory in base_directory
 do
     # parent_directory="${base_directory}/${parent_directory_prefix}$"
+    parent_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids/sub-606855754171"
     nested_directory="${parent_directory}/dwi_temp"
     
     mkdir -p "${nested_directory}"
