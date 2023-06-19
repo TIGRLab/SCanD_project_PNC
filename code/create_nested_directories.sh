c0459a5a41390ce9bc802a3840da12a5109bae1b
sunclara <132281878+sunclara@users.noreply.github.com>
2023-06-19 14:29:23 -0400

    Update create_nested_directories.sh
    removed test version "sub--"

--- code/create_nested_directories.sh
+++ code/create_nested_directories.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 base_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids"
-parent_directory_prefix="sub--"
+parent_directory_prefix="sub-"
 
 # Iterate through the sub--X directories
 for sub_directory in ${base_directory}/${parent_directory_prefix}*
@@ -12,5 +12,5 @@ do
     nested_directory="${sub_directory}/dwi_temp"
     
     mkdir -p "${nested_directory}"
-    echo "Creating nested dwi_temp directories"
+    # echo "Creating nested dwi_temp directories"
 done
