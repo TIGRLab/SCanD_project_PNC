6ef7a25727b9c9bf97e58a3f4a15d15f15c55629
sunclara <132281878+sunclara@users.noreply.github.com>
2023-07-06 16:31:29 -0400

    Update 02_qsiprep_scinet.sh
    Removed --dwi-only flag because T1w processing is required

--- code/02_qsiprep_scinet.sh
+++ code/02_qsiprep_scinet.sh
@@ -64,7 +64,6 @@ singularity run --cleanenv \
     --participant_label ${SUBJECTS} \
     -w /work \
     --skip-bids-validation \
-    --dwi-only \
     --omp-nthreads 8 \
     --nthreads 40 \
     --mem-mb 15000 \
