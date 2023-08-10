67e3a0475297a2ce96a46fb79c1fc5bba74cea23
sunclara <132281878+sunclara@users.noreply.github.com>
2023-08-10 18:38:49 -0400

    Update 02_fmriprep_func_scinet.sh
    changed name of tsv file containing participant bids IDs

--- code/02_fmriprep_func_scinet.sh
+++ code/02_fmriprep_func_scinet.sh
@@ -49,7 +49,7 @@ mkdir -vp ${OUTPUT_DIR} ${WORK_DIR} # ${LOCAL_FREESURFER_DIR}
 ## get the subject list from a combo of the array id, the participants.tsv and the chunk size
 bigger_bit=`echo "($SLURM_ARRAY_TASK_ID + 1) * ${SUB_SIZE}" | bc`
 # SUBJECTS=`sed -n -E "s/sub-(\S*)\>.*/\1/gp" ${BIDS_DIR}/participants.tsv | head -n ${bigger_bit} | tail -n ${SUB_SIZE}`
-SUBJECTS=`sed -n -E "s/sub-(\S*)\>.*/\1/gp" ${BIDS_DIR}/no_qc_fmri.tsv | head -n ${bigger_bit} | tail -n ${SUB_SIZE}` # updated 
+SUBJECTS=`sed -n -E "s/sub-(\S*)\>.*/\1/gp" ${BIDS_DIR}/new_no_qc_fmri.tsv | head -n ${bigger_bit} | tail -n ${SUB_SIZE}` # updated 
 ## set singularity environment variables that will point to the freesurfer license and the templateflow bits
 # Make sure FS_LICENSE is defined in the container.
 export SINGULARITYENV_FS_LICENSE=/home/fmriprep/.freesurfer.txt
