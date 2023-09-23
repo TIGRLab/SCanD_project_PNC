edfffd5c631dd31ffa5604cfdefdcc6a4ba693a0
sunclara <132281878+sunclara@users.noreply.github.com>
2023-09-23 18:36:41 -0400

    Add script create_nested_directories.sh

---
+++ code/helper/create_nested_directories.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Description: creates nested a "dwi_temp" folder within each bids subject folder
+
+base_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids"
+parent_directory="/scratch/a/arisvoin/clarasun/SCanD_project_PNC/data/local/bids/sub-*"
+
+for parent_directory in base_directory
+do
+    # parent_directory="${base_directory}/${parent_directory_prefix}$"
+    nested_directory="${parent_directory}/dwi_temp"
+    
+    mkdir -p "${nested_directory}"
+done
